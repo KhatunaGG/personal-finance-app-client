ee9347373aef2db658c12ffc5084ba9cb14bdc7f
Gagloshvili <KGagloshvili@oriflame.com>
2024-12-26 23:25:24 +0400

    added modal component: fix

--- src/app/components/__molecules/modal/Modal.tsx
+++ src/app/components/__molecules/modal/Modal.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import AddModal from "../../__organism/pageModal/PageModal";
 
 const Modal = () => {
-    const [modal, setModal] = useState(false)
-    const [activePage, setActivePage] = useState(false)
+    // const [modal, setModal] = useState(false)
+    // const [activePage, setActivePage] = useState(false)
   return (
     <section className="absolute inset-0 bg-black/50 w-full h-full z-10 ">
       <AddModal />
