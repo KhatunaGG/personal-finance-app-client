8faf934228d8fa268b89fcddfdfb669e44395c20
Gagloshvili <KGagloshvili@oriflame.com>
2024-12-27 12:39:53 +0400

    fixed: folder structure

--- src/app/components/__molecules/modal/Modal.tsx
+++ src/app/components/__molecules/modal/Modal.tsx
@@ -1,12 +1,7 @@
 "use client";
-import { useContext } from "react";
 import AddModal from "../../__organism/pageModal/PageModal";
-import { GlobalContext } from "@/app/context/Context";
 
 const Modal = () => {
-  const context = useContext(GlobalContext);
-  const { isModal } = context || {};
-
   return (
     <section className="absolute inset-0 bg-black/50 w-full h-full z-10 ">
       <AddModal />
