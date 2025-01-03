5a9644c2d8497d1c576894e4027a228063ceb0f3
Gagloshvili <KGagloshvili@oriflame.com>
2025-01-03 21:41:54 +0400

    added: budget/spending

--- src/app/components/__organism/budgets/Spending.tsx
+++ src/app/components/__organism/budgets/Spending.tsx
@@ -1,37 +1,45 @@
-import React from "react";
+import { GroupedCategory } from "@/app/hooks/use-categoryGrope";
 
-const Spending = () => {
+export type SpendingPropsType = {
+  groupedData: GroupedCategory[];
+};
+
+const Spending = ({ groupedData }: SpendingPropsType) => {
+  const slicedGroupedData =
+    groupedData.length < 4
+      ? groupedData
+      : groupedData.slice(groupedData.length - 4);
+
   return (
     <div className="w-full flex flex-col gap-y-2">
       <p className="text-[#201F24] text-[20px] font-bold">Spending Summary</p>
       <div>
-        <div className="flex flex-row gap-4 py-4">
-          <div className="w-[5px] bg-[#277C78] h-full min-h-[21px] rounded-sm"></div>
-          <div className="w-full flex flex-row items-center justify-between ">
-            <p className="text-[12px] font-normal text-[#696868]">
-              Entertainment
-            </p>
-            <div className="flex items-center gap-2">
-              <p className="text-[#201F24] font-bold text-[14px]">$159</p>
-              <p className="text-[#696868] font-normal text-[12px]">
-                of $50.00
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-row gap-4 py-4">
-          <div className="w-[5px] bg-[#277C78] h-full min-h-[21px] rounded-sm"></div>
-          <div className="w-full flex flex-row items-center justify-between ">
-            <p className="text-[12px] font-normal text-[#696868]">Bills</p>
-            <div className="flex items-center gap-2">
-              <p className="text-[#201F24] font-bold text-[14px]">$150.00</p>
-              <p className="text-[#696868] font-normal text-[12px]">
-                of $750.00
-              </p>
-            </div>
-          </div>
-        </div>
+        {slicedGroupedData.map((item, i) => {
+          if (item.spending < 0) {
+            return (
+              <div key={i} className="flex flex-row gap-4 py-4">
+                <div
+                  style={{ backgroundColor: item.color }}
+                  className="w-[5px] h-full min-h-[21px] rounded-sm"
+                ></div>
+                <div className="w-full flex flex-row items-center justify-between ">
+                  <p className="text-[12px] font-normal text-[#696868]">
+                    {item.category}
+                  </p>
+                  <div className="flex items-center gap-2">
+                    <p className="text-[#201F24] font-bold text-[14px]">
+                      ${Math.abs(item.spending).toFixed(2)}
+                    </p>
+                    <p className="text-[#696868] font-normal text-[12px]">
+                      of ${item.totalAmount.toFixed(2)}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          }
+          return null;
+        })}
       </div>
     </div>
   );
