015ef6e571d1ec61615230e1bf99ecd88560b192
Gagloshvili <KGagloshvili@oriflame.com>
2025-02-10 14:30:34 +0400

    some refuctoring of sidebar

--- src/app/components/__organism/sideBar/SideBar.tsx
+++ src/app/components/__organism/sideBar/SideBar.tsx
@@ -117,6 +117,9 @@ import { IconType } from "../../__atoms/billIcon/BillIcon";
 import { Logo, LogOut, MinimizeMenu } from "../../__atoms";
 import useAccessToken from "@/app/hooks/use-toke";
 import { GlobalContext } from "@/app/context/Context";
+import Image from "next/image";
+
+
 
 export type NavLinkType = {
   name: string;
@@ -127,10 +130,7 @@ const SideBar = () => {
   const path = usePathname();
   const { user } = useAccessToken();
   // const [minimize, setMinimize] = useState(false);
-  const context = useContext(GlobalContext)
-
-
-
+  const context = useContext(GlobalContext);
 
   const normalizePath = (str: string): string => {
     return decodeURIComponent(str)
@@ -151,19 +151,27 @@ const SideBar = () => {
     );
   };
 
-  if(!context) return null
-  const {minimize, setMinimize} = context;
-
-
+  if (!context) return null;
+  const { minimize, setMinimize } = context;
 
   return (
     <section
-      // style={{ width: minimize ? "300px" : "88px" }}
-      className={`absolute z-10 bottom-0 left-0 max-h-[52px] w-full md:max-h-[74px] lg:left-0 lg:top-0  lg:min-h-full h-screen bg-[#201F24] lg:rounded-t-xl lg:rounded-tl-none lg:rounded-r-3xl order-last lg:order-none ${minimize ? "lg:w-[88px]" : "lg:w-[300px]"} transition-all duration-300 ease-in-out`}
+      className={`absolute z-10 bottom-0 left-0 max-h-[52px] w-full md:max-h-[74px] lg:left-0 lg:top-0  lg:min-h-full h-screen bg-[#201F24] lg:rounded-t-xl lg:rounded-tl-none lg:rounded-r-3xl order-last lg:order-none ${
+        minimize ? "lg:w-[88px]" : "lg:w-[300px]"
+      } transition-all duration-300 ease-in-out`}
     >
       <div className="text-[#B3B3B3] w-full px-10 lg:px-0 flex flex-row items-end justify-around lg:flex-col">
-        <div className="w-full pl-[11.59%] py-10 hidden lg:flex">
-          <Logo />
+        <div className={`w-full pl-[11.59%] py-10 hidden lg:flex ${minimize && "flex items-center justify-center pl-0 "}`}>
+          {/* <Logo /> */}
+          {minimize ? (
+
+          <div className="w-[12.48px] h-[21.44px]">
+                <Image src={"/assets/f.svg"} alt={"f-logo"} width={12.48} height={21.44} />
+          </div>
+          ): (
+            <Logo />
+          )}
+      
         </div>
         <div className="w-full flex flex-row justify-between lg:flex-col md:gap-1">
           {navLinks.map((link, i) => {
@@ -193,7 +201,11 @@ const SideBar = () => {
                   ></div>
                   <div className="w-full flex flex-col items-center justify-center pt-2 lg:justify-start lg:flex-row lg:items-center lg:gap-4 lg:py-4 lg:pl-[9.78%]">
                     <link.icon isLinkActive={isLinkActive} />
-                    <p className={`hidden md:flex md:text-[12px] lg:text-base ${minimize && "lg:hidden"}`}>
+                    <p
+                      className={`hidden md:flex md:text-[12px] lg:text-base ${
+                        minimize && "lg:hidden"
+                      }`}
+                    >
                       {link.name ? link.name : ""}
                     </p>
                   </div>
@@ -204,11 +216,17 @@ const SideBar = () => {
           })}
         </div>
 
-        {/* <div className="w-full flex flex-col items-center justify-center"> </div> */}
-
-        <div className={`${minimize && "hidden "} w-full py-4 px-6 flex flex-col gap-4`}>
+        <div
+          className={`w-full hidden py-4 px-6 lg:flex flex-col gap-4 min-h-[125px] ${
+            minimize && "lg:items-start"
+          }`}
+        >
           <div className="w-full h-[1px] bg-[#B3B3B3]"></div>
-          <div className="flex flex-col items-start gap-2 text-xs lg:pl-[9%]">
+          <div
+            className={`flex flex-col items-start gap-2 text-xs lg:pl-[9%] ${
+              minimize && "hidden "
+            } `}
+          >
             <p className="">
               User:{" "}
               <span className="text-[#277C78] text-sm">{user?.userName}</span>{" "}
@@ -217,12 +235,24 @@ const SideBar = () => {
               Email:{" "}
               <span className="text-[#277C78] text-sm"> {user?.email} </span>{" "}
             </p>
-            <div className="w-[20px] h-[20px] relative group flex items-center gap-2">
-              <LogOut />
-              <span className="absolute left-[60px] top-1/2 transform -translate-y-1/2  -translate-x-1/2 bottom-full shadow-lg mb-1 text-xs text-[#B3B3B3] bg-[#201F24] p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
-                Log out
-              </span>
-            </div>
+          </div>
+
+          <div
+            className={`w-[20px] h-[20px] relative group flex items-center gap-2 ${
+              minimize && "pt-[84px]"
+            }`}
+          >
+            <LogOut />
+            {/* <span className="absolute left-[60px] top-1/2 transform -translate-y-1/2  -translate-x-1/2 bottom-full shadow-lg mb-1 text-xs text-[#B3B3B3] bg-[#201F24] p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap"> */}
+            <span
+              className={`${
+                minimize
+                  ? "lg:left-[20px] lg:top-[25px]"
+                  : "lg:left-[60px] lg:top-1/2"
+              } absolute top-1/2 transform -translate-y-1/2  -translate-x-1/2 bottom-full shadow-lg mb-1 text-xs text-[#B3B3B3] bg-[#201F24] p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap`}
+            >
+              Log out
+            </span>
           </div>
         </div>
 
@@ -231,8 +261,8 @@ const SideBar = () => {
           className="hidden lg:w-[92%] lg:rounded-r-lg lg:flex flex-row gap-[10px]"
         >
           {/* <div className="flex flex-row items-center gap-4 py-4 pl-[11.81%] pt-[calc(100vh-432px)]"> */}
-          <div className="flex flex-row items-center gap-4 py-4 pl-[11.81%] pt-[calc(100vh-561px)]">
-            <MinimizeMenu />
+          <div className={`flex flex-row items-center gap-4 py-4 pl-[11.81%] pt-[calc(100vh-600px)] ${minimize && "w-full pl-[16%]"}`}>
+            <MinimizeMenu minimize={minimize} />
             <p className={`${minimize && "lg:hidden"}`}>Minimize Menu</p>
           </div>
         </button>
